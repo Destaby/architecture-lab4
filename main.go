b677f04747fed2243d7278103b08bc5b372a31f3
Destaby <rudik.nick.nick@gmail.com>
2021-12-16 20:50:12 +0200

    add queue

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"strings"
 	"os"
+	"sync"
 )
 
 type Command interface {
@@ -14,20 +15,54 @@ type Handler interface {
 	Post(cmd Command)
 }
 
-type Loop struct {
+type cmdQueue struct {
+	sync.Mutex
+	c []Command
+}
 
+func (q *cmdQueue) push(cmd Command) {
+	q.Lock()
+	defer q.Unlock()
+	q.c = append(q.c, cmd)
+}
+
+func (q *cmdQueue) pull() Command {
+	q.Lock()
+	defer q.Unlock()
+	res := q.c[0]
+	q.c[0] = nil
+	q.c = q.c[1:]
+	return res
+}
+
+type Loop struct {
+	queue *cmdQueue
+	stopPulling bool
+	stop chan struct{}
 }
 
 func (l *Loop) Post(cmd Command) {
-	cmd.Execute(l);
+	l.queue.push(cmd)
 }
 
 func (l *Loop) Start() {
-
+	l.queue = &cmdQueue{}
+	l.stop = make(chan struct{})
+	go func() {
+		for {
+			if len(l.queue.c) == 0 && l.stopPulling {
+				break
+			}
+			cmd := l.queue.pull()
+			cmd.Execute(l)		
+		}
+		l.stop <- struct{}{}
+	}()
 }
 
 func (l *Loop) AwaitFinish() {
-
+	l.stopPulling = true
+	<-l.stop
 }
 
 func parse(line string) Command {
@@ -42,7 +77,7 @@ func parse(line string) Command {
 		if len(parts) != 2 {
 			errMsg[0] = "\nReason: Comand should have one argument"
 		}
-		if parts[0] != "print" || parts[0] != "palindrom" {
+		if parts[0] != "print" && parts[0] != "palindrom" {
 			errMsg[1] = "\nReason: Unknown command"
 		}
 		cmd = &printCommand{arg: "SYNTAX ERROR in line: " + line + errMsg[0] + errMsg[1]}
@@ -52,6 +87,7 @@ func parse(line string) Command {
 
 func main() {
 	loop := new(Loop) 
+
 	loop.Start() 
 	
 	if input, err := os.Open("test.txt"); err == nil {  
@@ -62,6 +98,6 @@ func main() {
 			cmd := parse(commandLine) // parse the line to get a Command  
 			loop.Post(cmd) 
 		} 
-	} 
-	loop.AwaitFinish() 
+	}
+	loop.AwaitFinish()
 }
